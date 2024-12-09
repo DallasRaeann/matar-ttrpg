0ba0ca00b91c6a0b578a173bbb2027efb6dafc5c
Dallas Schoten <dallas.scholten@gmail.com>
2024-12-08 21:59:47 -0500

    Quartz sync: Dec 8, 2024, 9:59 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -158,12 +158,13 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
-          <>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
-          </>
+            {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
+                <>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
+                    <link rel="stylesheet" href="" />
+                    <link rel="stylesheet" href="" />
         )}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* OG/Twitter meta tags */}
